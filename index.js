6015067386b71fbbbb6a6de4af6d460ebcc4cf76
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-20 18:02:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const { prefix, token, owner} = require('./config.json');
 const { NekoBot } = require("nekobot-api");
 const api = new NekoBot();
 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 

1cdc3f0ecbd757e28b6cceefc4ddcd910a36ec07
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-20 23:14:59 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,6 +4,8 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args, client) {
 let d = client.ws.ping / 1000
-		message.channel.send(`Понг! задержка бота ${d}секунд`);
+		message.channel.send(`Загрузка <a:loading:856264634360266793>`).then(message => {
+                setTimeout(()=>{ message.edit(`Понг! Задержка бота ${d}секунд`) }, 3000)
+              })
 	},
 };

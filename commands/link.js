d0f271131d337de591ceb0255c621fb75677adfb
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-21 23:50:39 +0300

    Create link.js

---
+++ commands/link.js
@@ -0,0 +1,14 @@
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+const { randomInt } = require('crypto');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs')
+const path = require('path')
+module.exports = {
+	name: 'секс',
+	description: 'eval',
+	cooldown: 5,
+	execute(message, args, client, api) {
+client.generateInvite({
+ permissions: ['ADMINISTRATOR']}).then(link => message.channel.send(`Ссылка на бота ${link}`))
+  }

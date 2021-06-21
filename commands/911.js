5b73f12ecedc02e6057890635d1686f8253b1b09
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-21 23:53:32 +0300

    Create 911.js

---
+++ commands/911.js
@@ -0,0 +1,18 @@
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+const { randomInt } = require('crypto');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs')
+const path = require('path')
+module.exports = {
+	name: '911',
+	description: 'eval',
+	cooldown: 5,
+	execute(message, args, client, api) {
+const moder = '732196406491611157'
+const embed = new Discord.MessageEmbed()
+.setTitle('Время вызова')
+.setColor('5865f2')
+.setTimestamp();
+message.channel.send(`<@${message.author.id}> вызвал <@&${moder}>`, { embed} )
+  }}

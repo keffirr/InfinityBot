6d69d325f6608c523eb3bcb2ea616c613fc6a401
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-22 00:02:49 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,20 @@
+const { MessageButton, MessageActionRow } = require('discord-buttons');
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	cooldown: 5,
 	execute(message, args, client) {
+		let button = new MessageButton()
+  .setStyle('blurple')
+  .setLabel('Обновить') 
+  .setID('click_to_function') 
 let d = client.ws.ping / 1000
 		message.channel.send(`Загрузка <a:loading:856264634360266793>`).then(message => {
-                setTimeout(()=>{ message.edit(`Понг! Задержка бота ${d}секунд`) }, 3000)
+                setTimeout(()=>{ message.edit(`Понг! Задержка бота ${d}секунд`, button) }, 3000)
               })
+client.on('clickButton', async (button) => {
+    if(button.id === 'click_to_function') {
+button.message.edit(`Понг! Задержка бота ${d}секунд`)
+    }})
 	},
 };

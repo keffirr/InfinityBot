edf8310d9b1e899113e4c9e59d2f427b75b6f1fb
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-21 20:29:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ const path = require('path');
 var server = express();
 var port = process.env.PORT || 8080; // <== this is must
 
-server.get('/', (req, res) => {
+server.get('/status', (req, res) => {
 
         res.send(`<p>'Бот включен\nи нагружен на ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB`)
 })

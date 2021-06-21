7b8c165b1ebf46a8c0a1f296497f2c95ef359e3e
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-21 23:48:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
     const api = new NekoBot();
     
     const client = new Discord.Client();
+const disbut = require('discord-buttons')(client)
+const { MessageButton, MessageActionRow } = require('discord-buttons');
     client.commands = new Discord.Collection();
     
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));

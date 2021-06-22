76093c965008d07a89078956238e2e5b6797cac2
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-23 00:06:58 +0300

    Фикс v2

--- commands/ping.js
+++ commands/ping.js
@@ -9,12 +9,10 @@ module.exports = {
   .setLabel('Обновить') 
   .setID('click_to_function') 
 let d = client.ws.ping / 1000
-		message.channel.send(`Понг! Задержка бота ${d}секунд `, button).then(message => {
-                setTimeout(()=>{ button.message.edit(`Понг! Задержка бота ${d}секунд`) }, 3000)
-              })
+		message.channel.send(`Понг! Задержка бота ${d}секунд `, button)
 client.on('clickButton', async (button) => {
     if(button.id === 'click_to_function') {
-button.reply.send(`Понг! Задержка бота ${d}секунд`, { type: 7 })
+button.message.edit(`Понг! Задержка бота ${d}секунд`)
 await button.defer()
     }})
 	},

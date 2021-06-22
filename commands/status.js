18eb970567c5855b089dc4fb6cfda9efa986416a
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-22 23:12:12 +0300

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -9,13 +9,5 @@ module.exports = {
 	description: 'eval',
 	cooldown: 5,
 	execute(message, args, client, api) {
-const embed = new Discord.MessageEmbed()
-.setTitle('Infinity Bot Status')
-.setDescription(`Бот нагружен на ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB`)
-.setThumbnail('https://images-ext-2.discordapp.net/external/Z4oniAqvvYvHpAF-b40h3AT2PsCoRoUf-uyiU1GHvk8/https/cdn.discordapp.com/avatars/851945606799949844/236cf6c8e0ffea770b2e4eb24992b28b.png')
-.setURL('https://status.romoz.ml')
-.setTimestamp()
-.setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL({ format: "png", dynamic: true })}`)
-.setColor('FFFFFF');
-message.channel.send(`Посмотрите статиску на сайте ** https://status.romoz.ml ** , более подробная на ** https://status.romoz.ml/подробная **`, { embed })
+message.channel.send(`Посмотрите статистику на сайте ** https://status.romoz.ml, ** более подробную на ** https://status.romoz.ml/подробная **`)
 }}

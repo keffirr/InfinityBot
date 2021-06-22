bfd1e9987db9637218711a3f22ed348e7cedd49e
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-22 14:50:17 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,44 +1,45 @@
-const { prefix } = require('../config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 module.exports = {
 	name: 'help',
-	description: 'List all of my commands or info about a specific command.',
+	description: 'Список всех команд бота.',
 	aliases: ['commands'],
 	usage: '[command name]',
 	cooldown: 5,
 	execute(message, args) {
-        const data = [];
-        const { commands } = message.client;
-        
-        if (!args.length) {
-            const name = args[0];
-            const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+        const prefix = '!'
+		const data = [];
+		const { commands } = message.client;
 
-            if (!command) {
-        	return message.reply('Это не правильная команда!');
-        }
-
-         data.push(`**Название:** ${command.name}`);
-
-         if (command.aliases) data.push(`**Синонимы:** ${command.aliases.join(', ')}`);
-         if (command.description) data.push(`**Описание:** ${command.description}`);
-         if (command.usage) data.push(`**Использование:** ${prefix}${command.name} ${command.usage}`);
-
-         data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-            message.channel.send(data, { split: true });
-            data.push('Это список моих команд:');
+		if (!args.length) {
+            data.push('Вот список всех моих команд:');
             data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nВы можете написать \`${prefix}help [название команды]\` чтобы получить информацию о конкретной команде!`);
-            
+            data.push(`\nТы можешь написать \`${prefix}help [command name]\` чтобы получить информацию о команде!`);
+        
             return message.author.send(data, { split: true })
                 .then(() => {
                     if (message.channel.type === 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
+                    message.reply('Я отправил список команд в личные сообщения.');
                 })
                 .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+                    console.error(`Не удалось отправить список команд ${message.author.tag}.\n`, error);
+                    message.reply('Почему-то не вышло отправить список команд! Ты закрыл личные сообщения?');
                 });
-        }
+		}
+
+        const name = args[0].toLowerCase();
+const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+if (!command) {
+	return message.reply('Это не верная команда!');
+}
+
+data.push(`**Name:** ${command.name}`);
+
+if (command.aliases) data.push(`**Псевдонимы:** ${command.aliases.join(', ')}`);
+if (command.description) data.push(`**Описание:** ${command.description}`);
+if (command.usage) data.push(`**Использование:** ${prefix}${command.name} ${command.usage}`);
+
+message.channel.send(embed);
 	},
 };

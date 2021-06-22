70c9c0bf7d317c63f4157adf365f59192415beac
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-22 09:51:24 +0300

    Update заметка.js

--- commands/заметка.js
+++ commands/заметка.js
@@ -9,7 +9,7 @@ module.exports = {
         usage: 'content',
 	cooldown: 5,
 	execute(message, args) {
-if (message.content === 'заметка посмотреть') return
+if (message.content === `${prefix}заметка посмотреть`) return
 const zametka = fs.writeFileSync(`${message.author.username}.txt`, `${message.content.split(" ").slice(1).join(" ")}`)
 zametka
 message.channel.send(`Ваша заметка сохранена! Вот как она выглядит: **${fs.readFileSync(`${message.author.username}.txt`)}**\nЕсли вы хотите посмотреть свою заметку позже, напишите команду \`!заметка посмотреть\``)

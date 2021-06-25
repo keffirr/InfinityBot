6d355b293fbcc8f03fa0747bf165d0ff6321495b
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-25 20:51:29 +0300

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -63,12 +63,12 @@ await button.reply.send(`Роль <@&${give}> успешно выдана для
                     member.roles.add(role);
                     await button.reply.send(`Роль <@&${give}> успешно выдана для <@${button.clicker.user.id}>!`, true)
     }
-        if(button.id === 'rule') {
+if(button.id === 'rule') {
         const give = '771297776042639360'
                     const role = button.guild.roles.cache.get(give);
                     const member = message.guild.members.cache.get(button.clicker.user.id);
-                    if(message.member.roles.cache.has(give)) return message.guild.members.cache.get(button.clicker.user.id).send('Вы уже прошли верификацию, повторно ее пройти нельзя!'), await button.defer()
                     member.roles.add(role);
+                    if(message.member.roles.cache.has(give)) return message.guild.members.cache.get(button.clicker.user.id).send('Вы уже прошли верификацию, повторно ее пройти нельзя!'), await button.defer()
 await button.reply.send(`<@${button.clicker.user.id}> успешно прошел верификацию!`, true)
     }
 })

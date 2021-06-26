9c56de75a56c950223ccc50d74a9d356c999a104
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-26 11:28:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -166,6 +166,18 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
   });
 }
 
+client.on('clickButton', async (button) => {
+    if(button.id === 'rule') {
+        const give = '771297776042639360'
+                    const role = button.guild.roles.cache.get(give);
+                    const member = message.guild.members.cache.get(button.clicker.user.id);
+                    member.roles.add(role);
+                    if(message.member.roles.cache.has(give)) return message.guild.members.cache.get(button.clicker.user.id).send('Вы уже прошли верификацию, повторно ее пройти нельзя!'), await button.defer()
+                    button.message.edit(`${eval(1 + button.message.content[0] - 9)} человека прошли верификацию`, { embed: embed, components: [ buttonRow ] })
+await button.reply.send(`<@${button.clicker.user.id}> успешно прошел верификацию!`, true)
+    }
+})
+
 client.login(process.env.BOT_TOKEN); 
 	const express = require('express');
 const path = require('path');

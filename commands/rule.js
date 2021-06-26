04e3c0a452c6233993de21f32bf22b1d70f038a2
RomozML <81045088+RomozML@users.noreply.github.com>
2021-06-26 03:58:14 +0300

    Update rule.js

--- commands/rule.js
+++ commands/rule.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
     const MessageEmbed = require('discord.js');
 const client = new Discord.Client();
 module.exports = {
-    name: 'role',
+    name: 'rule',
     description: 'Ping!',
     cooldown: 5,
     execute(message, args, client, MessageEmbed, Discord) {

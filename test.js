a688479f2f126d491a12f961c9d56c34afffd6d8
RomozML <81045088+RomozML@users.noreply.github.com>
2021-07-03 19:42:17 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',

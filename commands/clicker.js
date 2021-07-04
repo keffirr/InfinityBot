42bed8b6794e806e5eae69dac594ab683e6bd5f7
RomozML <81045088+RomozML@users.noreply.github.com>
2021-07-04 13:08:13 +0300

    Update clicker.js

--- commands/clicker.js
+++ commands/clicker.js
@@ -11,11 +11,11 @@ let button = new MessageButton()
 .setStyle('red') 
 .setLabel(1)
 .setID(`clicker`);
-    message.channel.send(`${button.label} раз нажали на кнопку`, button)
+    message.channel.send(`${button.label}`, button)
 client.on('clickButton', async (button) => {
 let button2 = new MessageButton()
 .setStyle('red') 
-.setLabel(+button.message.content[0] + +1)
+.setLabel(+button.message.content + +1)
 .setID(`clicker`);
     if(button.id === 'clicker') {
 button.reply.send(`${button2.label} раз нажали на кнопку`, button2)

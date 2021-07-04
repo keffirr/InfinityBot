8ba36fb9cfe177bf63d674ae4625aeb6c084232d
RomozML <81045088+RomozML@users.noreply.github.com>
2021-07-05 02:28:39 +0300

    Update rule.js

--- commands/rule.js
+++ commands/rule.js
@@ -20,7 +20,7 @@ module.exports = {
 	.setColor('FFFFFF')
 
 	// Send a message using the webhook
-	message.channel.send(`Верификацию прошло 1 человека`, { embed: embed, components: [ buttonRow ] }); 
+	message.channel.send(`1 человека прошли верификацию`, { embed: embed, components: [ buttonRow ] }); 
 client.on('clickButton', async (button) => {
     if(button.id === 'rule') {
         const give = '771297776042639360'
@@ -28,7 +28,7 @@ client.on('clickButton', async (button) => {
                     const member = message.guild.members.cache.get(button.clicker.user.id);
                     member.roles.add(role);
                     if(message.member.roles.cache.has(give)) return message.guild.members.cache.get(button.clicker.user.id).send('Вы уже прошли верификацию, повторно ее пройти нельзя!')
-                    button.message.edit(`Верификацию прошло ${eval(1 + button.message.content.split(" ").slice(3).join(" "))} человека`, { embed: embed, components: [ buttonRow ] })
+                    button.message.edit(`${eval(1 + button.message.content[0] - 9)} человека прошли верификацию`, { embed: embed, components: [ buttonRow ] })
 await button.reply.send(`<@${button.clicker.user.id}> успешно прошел верификацию!`, true)
     }
 })

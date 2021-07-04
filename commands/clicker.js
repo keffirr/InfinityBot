1080f99fdc457773124936fb6ecd2d44648ce398
RomozML <81045088+RomozML@users.noreply.github.com>
2021-07-04 13:33:16 +0300

    Update clicker.js

--- commands/clicker.js
+++ commands/clicker.js
@@ -7,18 +7,27 @@ module.exports = {
     description: 'Ping!',
     cooldown: 5,
     execute(message, args, client, MessageEmbed, Discord) {
+const colors = [
+'red',
+'green',
+'blurple',
+]
+const randomIndex = Math.floor(Math.random() * (colors.length - 1) + 1)
+const randomColor = colors[randomIndex]
 let button = new MessageButton()
-.setStyle('red') 
+.setStyle(randomColor) 
 .setLabel(1)
 .setID(`clicker`);
-    message.channel.send(`${button.label}`, button)
+    message.channel.send(`${button.label}`, button).then(message => {
+                message.delete({ timeout: 10000 })
 client.on('clickButton', async (button) => {
 let button2 = new MessageButton()
-.setStyle('red') 
+.setStyle(randomColor) 
 .setLabel(+button.message.content + +1)
 .setID(`clicker`);
     if(button.id === 'clicker') {
-button.reply.send(`${button2.label}`, button2)
+button.reply.send(`${button2.label}`, button2).then(message => {
+                message.delete({ timeout: 10000 })
 }
 })
 }}
